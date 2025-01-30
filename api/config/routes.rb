14d023e9d7384bc9e5aef41792e0398e0eb4a3ad
Thiago Pelizoni <thiago.pelizoni@gmail.com>
2025-01-30 16:15:46 -0300

    Ajustando as rotas dos pedidos

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -4,5 +4,26 @@ Rails.application.routes.draw do
   
   root "pedidos#index"
 
+  resources :pedidos do
+    collection do
+      get 'prontos', to: 'pedidos#prontos'
+      get 'recebidos', to: 'pedidos#recebidos'
+      get 'em-preparacao', to: 'pedidos#em_preparacao'
+      get 'finalizados', to: 'pedidos#finalizados'
+      get 'pagamento-confirmado', to: 'pedidos#pagamento_confirmado'
+      get 'pagamento-em-aberto', to: 'pedidos#pagamento_em_aberto'
+      get 'pagamento-recusado', to: 'pedidos#pagamento_recusado'
+    end
+
+    member do
+      put 'pagar', to: 'pedidos#pagar'
+      put 'receber', to: 'pedidos#receber'
+      put 'preparar', to: 'pedidos#preparar'
+      put 'pronto', to: 'pedidos#pronto'
+      put 'finalizar', to: 'pedidos#finalizar'
+      get 'qr-code', to: 'pedidos#qr_code'
+    end
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
