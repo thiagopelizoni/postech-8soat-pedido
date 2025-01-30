871f8169d7c47959aac7b468ad04c71f1dad5f02
Thiago Pelizoni <thiago.pelizoni@gmail.com>
2025-01-30 16:08:58 -0300

    Removendo chamadas de objetos dependentes não mais existentes.

--- api/app/serializers/pedido_serializer.rb
+++ api/app/serializers/pedido_serializer.rb
@@ -1,11 +1,5 @@
 class PedidoSerializer < ActiveModel::Serializer
-  attributes :id, :valor, :status, :observacao, :data, :data_status, :pagamento
-  belongs_to :cliente, serializer: ClienteSerializer
-  has_many :produtos, serializer: ProdutoSerializer
-
-  def produtos
-    object.produtos.map { |produto_id| Produto.find(produto_id) }
-  end
+  attributes :id, :cliente, :produtos, :valor, :status, :observacao, :data, :data_status, :pagamento
 
   def data
     object.created_at.strftime("%d/%m/%Y %H:%m:%S")
